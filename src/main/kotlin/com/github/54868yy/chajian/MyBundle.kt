895b5b39a479c7622907adbbc05688098c051c31
GitHub Action <action@github.com>
2020-11-04 08:56:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/54868yy/chajian/MyBundle.kt
+++ src/main/kotlin/com/github/54868yy/chajian/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.54868yy.chajian
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/54868yy/chajian/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/54868yy/chajian/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.54868yy.chajian.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.54868yy.chajian.MyBundle
 
 class MyApplicationService {
 
